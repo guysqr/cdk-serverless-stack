04cca9abd29681d0231a0e8c8db1add4ce3c17db
Guy Morton <guy@alchemy.com.au>
2020-06-14 14:04:53 +1000

    updated lambdastack

--- cdk_pipelines/cdk_lambda_stack.py
+++ cdk_pipelines/cdk_lambda_stack.py
@@ -1,23 +1,32 @@
-from aws_cdk import core, aws_codedeploy as codedeploy, aws_lambda as lambda_
+from aws_cdk import core, aws_codedeploy as codedeploy, aws_apigateway as apigateway, aws_lambda as lambda_
+
 
 class LambdaStack(core.Stack):
-  def __init__(self, app: core.App, id: str, **kwargs):
-    super().__init__(app, id, **kwargs)
+    def __init__(self, app: core.App, id: str, **kwargs):
+        super().__init__(app, id, **kwargs)
 
-    self.lambda_code = lambda_.Code.from_cfn_parameters()
-      
-    func = lambda_.Function(self, "Lambda",
-                            code=self.lambda_code,
-                            handler="index.handler",
-                            runtime=lambda_.Runtime.NODEJS_10_X,
-    )
-      
-    version = func.latest_version
-    alias = lambda_.Alias(self, "LambdaAlias",
-                          alias_name="Prod", version=version)
-      
-    codedeploy.LambdaDeploymentGroup(self, "DeploymentGroup",
-        alias=alias,
-        deployment_config=
-            codedeploy.LambdaDeploymentConfig.LINEAR_10_PERCENT_EVERY_1_MINUTE
-    )
+        self.lambda_code = lambda_.Code.from_cfn_parameters()
+
+        func = lambda_.Function(self, "Lambda",
+                                code=self.lambda_code,
+                                handler="index.handler",
+                                runtime=lambda_.Runtime.NODEJS_10_X,
+                                )
+
+        api = apigateway.RestApi(self, "lambda-service",
+                                 rest_api_name="Lambda Service",
+                                 description="This service serves the lambda.")
+
+        get_lambda_integration = apigateway.LambdaIntegration(func,
+                                                              request_templates={"application/json": '{ "statusCode": "200" }'})
+
+        api.root.add_method("GET", get_lambda_integration)   # GET /
+
+        version = func.latest_version
+        alias = lambda_.Alias(self, "LambdaAlias",
+                              alias_name="Prod", version=version)
+
+        codedeploy.LambdaDeploymentGroup(self, "DeploymentGroup",
+                                         alias=alias,
+                                         deployment_config=codedeploy.LambdaDeploymentConfig.LINEAR_10_PERCENT_EVERY_1_MINUTE
+                                         )
