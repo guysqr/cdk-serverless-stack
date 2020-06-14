b7f4ca52f99e85bdfb4707192895f6f6eafd2570
Guy Morton <guy@alchemy.com.au>
2020-06-14 11:39:58 +1000

    updated lambda function

--- cdk_pipelines/cdk_pipelines_stack.py
+++ cdk_pipelines/cdk_pipelines_stack.py
@@ -4,6 +4,7 @@ from aws_cdk import (core, aws_codebuild as codebuild,
                      aws_codepipeline_actions as codepipeline_actions,
                      aws_lambda as lambda_, aws_s3 as s3)
 
+
 class PipelineStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, *,
@@ -11,45 +12,47 @@ class PipelineStack(core.Stack):
         super().__init__(scope, id, **kwargs)
 
         code = codecommit.Repository.from_repository_name(self, "ImportedRepo",
-                  "pipeline-stack");
+                                                          "pipeline-stack")
 
         cdk_build = codebuild.PipelineProject(self, "CdkBuild",
-                        build_spec=codebuild.BuildSpec.from_object(dict(
-                            version="0.2",
-                            phases=dict(
-                                install=dict(
-                                    commands=[
-                                        "python -m ensurepip --upgrade",
-                                        "python -m pip install --upgrade pip",
-                                        "python -m pip install --upgrade virtualenv",
-                                        "source .env/bin/activate",
-                                        "pip install -r requirements.txt"]),
-                                build=dict(commands=[
-                                    "cdk synth PipelineDeployingLambdaStack"])),
-                            artifacts={
-                                "base-directory": "cdk.out",
-                                "files": [
-                                    "LambdaStack.template.json"]},
-                            environment=dict(buildImage=
-                                codebuild.LinuxBuildImage.STANDARD_2_0))))
+                                              build_spec=codebuild.BuildSpec.from_object(dict(
+                                                  version="0.2",
+                                                  phases=dict(
+                                                      install=dict(
+                                                          commands=[
+                                                              "npm install -g aws-cdk",
+                                                              "python -m ensurepip --upgrade",
+                                                              "python -m pip install --upgrade pip",
+                                                              "python -m pip install --upgrade virtualenv",
+                                                              "virtualenv .env",
+                                                              ". .env/bin/activate",
+                                                              "pip install -r requirements.txt",
+                                                              "pip install aws_cdk.aws_codedeploy aws_cdk.aws_lambda aws_cdk.aws_codebuild aws_cdk.aws_codepipeline",
+                                                              "pip install aws_cdk.aws_codecommit aws_cdk.aws_codepipeline_actions aws_cdk.aws_s3"]),
+                                                      build=dict(commands=[
+                                                          "cdk synth PipelineDeployingLambdaStack"])),
+                                                  artifacts={
+                                                      "base-directory": "cdk.out",
+                                                      "files": [
+                                                          "LambdaStack.template.json"]},
+                                                  environment=dict(buildImage=codebuild.LinuxBuildImage.STANDARD_2_0))))
 
         lambda_build = codebuild.PipelineProject(self, 'LambdaBuild',
-                        build_spec=codebuild.BuildSpec.from_object(dict(
-                            version="0.2",
-                            phases=dict(
-                                install=dict(
-                                    commands=[
-                                        "cd lambda",
-                                        "npm install"]),
-                                build=dict(
-                                    commands="npm run build")),
-                            artifacts={
-                                "base-directory": "lambda",
-                                "files": [
-                                    "index.js",
-                                    "node_modules/**/*"]},
-                            environment=dict(buildImage=
-                                codebuild.LinuxBuildImage.STANDARD_2_0))))
+                                                 build_spec=codebuild.BuildSpec.from_object(dict(
+                                                     version="0.2",
+                                                     phases=dict(
+                                                         install=dict(
+                                                             commands=[
+                                                                 "cd lambda",
+                                                                 "npm install"]),
+                                                         build=dict(
+                                                             commands="npm run build")),
+                                                     artifacts={
+                                                         "base-directory": "lambda",
+                                                         "files": [
+                                                             "index.js",
+                                                             "node_modules/**/*"]},
+                                                     environment=dict(buildImage=codebuild.LinuxBuildImage.STANDARD_2_0))))
 
         source_output = codepipeline.Artifact()
         cdk_build_output = codepipeline.Artifact("CdkBuildOutput")
@@ -58,38 +61,38 @@ class PipelineStack(core.Stack):
         lambda_location = lambda_build_output.s3_location
 
         codepipeline.Pipeline(self, "Pipeline",
-            stages=[
-                codepipeline.StageProps(stage_name="Source",
-                    actions=[
-                        codepipeline_actions.CodeCommitSourceAction(
-                            action_name="CodeCommit_Source",
-                            repository=code,
-                            output=source_output)]),
-                codepipeline.StageProps(stage_name="Build",
-                    actions=[
-                        codepipeline_actions.CodeBuildAction(
-                            action_name="Lambda_Build",
-                            project=lambda_build,
-                            input=source_output,
-                            outputs=[lambda_build_output]),
-                        codepipeline_actions.CodeBuildAction(
-                            action_name="CDK_Build",
-                            project=cdk_build,
-                            input=source_output,
-                            outputs=[cdk_build_output])]),
-                codepipeline.StageProps(stage_name="Deploy",
-                    actions=[
-                        codepipeline_actions.CloudFormationCreateUpdateStackAction(
-                            action_name="Lambda_CFN_Deploy",
-                            template_path=cdk_build_output.at_path(
-                                "LambdaStack.template.json"),
-                            stack_name="LambdaDeploymentStack",
-                            admin_permissions=True,
-                            parameter_overrides=dict(
-                                lambda_code.assign(
-                                    bucket_name=lambda_location.bucket_name,
-                                    object_key=lambda_location.object_key,
-                                    object_version=lambda_location.object_version)),
-                            extra_inputs=[lambda_build_output])])
-                ]
-            )
+                              stages=[
+                                  codepipeline.StageProps(stage_name="Source",
+                                                          actions=[
+                                                              codepipeline_actions.CodeCommitSourceAction(
+                                                                  action_name="CodeCommit_Source",
+                                                                  repository=code,
+                                                                  output=source_output)]),
+                                  codepipeline.StageProps(stage_name="Build",
+                                                          actions=[
+                                                              codepipeline_actions.CodeBuildAction(
+                                                                  action_name="Lambda_Build",
+                                                                  project=lambda_build,
+                                                                  input=source_output,
+                                                                  outputs=[lambda_build_output]),
+                                                              codepipeline_actions.CodeBuildAction(
+                                                                  action_name="CDK_Build",
+                                                                  project=cdk_build,
+                                                                  input=source_output,
+                                                                  outputs=[cdk_build_output])]),
+                                  codepipeline.StageProps(stage_name="Deploy",
+                                                          actions=[
+                                                              codepipeline_actions.CloudFormationCreateUpdateStackAction(
+                                                                  action_name="Lambda_CFN_Deploy",
+                                                                  template_path=cdk_build_output.at_path(
+                                                                      "LambdaStack.template.json"),
+                                                                  stack_name="LambdaDeploymentStack",
+                                                                  admin_permissions=True,
+                                                                  parameter_overrides=dict(
+                                                                      lambda_code.assign(
+                                                                          bucket_name=lambda_location.bucket_name,
+                                                                          object_key=lambda_location.object_key,
+                                                                          object_version=lambda_location.object_version)),
+                                                                  extra_inputs=[lambda_build_output])])
+                              ]
+                              )
--- lambda/index.js
+++ lambda/index.js
@@ -1,26 +1,53 @@
 const AWSXRay = require('aws-xray-sdk-core')
-const AWS = AWSXRay.captureAWS(require('aws-sdk'))
-
+const AWS = require('aws-sdk')
 // Create client outside of handler to reuse
 const lambda = new AWS.Lambda()
 
 // Handler
-exports.handler = async function(event, context) {
-  event.Records.forEach(record => {
-    console.log(record.body)
-  })
+exports.handler = async function (event, context) {
   console.log('## ENVIRONMENT VARIABLES: ' + serialize(process.env))
   console.log('## CONTEXT: ' + serialize(context))
   console.log('## EVENT: ' + serialize(event))
-  
-  return getAccountSettings()
+  try {
+    let accountSettings = await getAccountSettings()
+    return formatResponse(serialize(accountSettings.AccountUsage))
+  } catch (error) {
+    return formatError(error)
+  }
 }
 
+var formatResponse = function (body) {
+  var response = {
+    "statusCode": 200,
+    "headers": {
+      "Content-Type": "application/json"
+    },
+    "isBase64Encoded": false,
+    "multiValueHeaders": {
+      "X-Custom-Header": ["My value", "My other value"],
+    },
+    "body": body
+  }
+  return response
+}
+
+var formatError = function (error) {
+  var response = {
+    "statusCode": error.statusCode,
+    "headers": {
+      "Content-Type": "text/plain",
+      "x-amzn-ErrorType": error.code
+    },
+    "isBase64Encoded": false,
+    "body": error.code + ": " + error.message
+  }
+  return response
+}
 // Use SDK client
-var getAccountSettings = function(){
+var getAccountSettings = function () {
   return lambda.getAccountSettings().promise()
 }
 
-var serialize = function(object) {
+var serialize = function (object) {
   return JSON.stringify(object, null, 2)
 }
