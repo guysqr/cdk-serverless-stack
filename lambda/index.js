b277cd18cdfb088575ef1e19737534b173114215
Guy Morton <guy@alchemy.com.au>
2020-06-14 14:52:02 +1000

    working on x-ray

--- lambda/index.js
+++ lambda/index.js
@@ -1,5 +1,6 @@
 const AWSXRay = require('aws-xray-sdk-core')
 const AWS = AWSXRay.captureAWS(require('aws-sdk'))
+
 // Create client outside of handler to reuse
 const lambda = new AWS.Lambda()
 
@@ -8,6 +9,10 @@ exports.handler = async function (event, context) {
   console.log('## ENVIRONMENT VARIABLES: ' + serialize(process.env))
   console.log('## CONTEXT: ' + serialize(context))
   console.log('## EVENT: ' + serialize(event))
+  AWSXRay.captureFunc('annotations', function (subsegment) {
+    subsegment.addAnnotation('Name', 'lambda-func');
+    subsegment.addAnnotation('UserID', event.userid);
+  });
   try {
     return formatResponse("<html><h1>Hi there</h1><body>Here is my output</body></html>")
   } catch (error) {
