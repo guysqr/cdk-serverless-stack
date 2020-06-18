39e98acd12502683699b2a437d410e7c6c7fb706
Guy Morton <guy@alchemy.com.au>
2020-06-18 13:29:40 +1000

    added tagging for Lambda stack

--- cdk_pipelines/cdk_pipelines_stack.py
+++ cdk_pipelines/cdk_pipelines_stack.py
@@ -13,7 +13,6 @@ class PipelineStack(core.Stack):
 
         repo_name = self.node.try_get_context("name") or "Repo"
         repo_count = self.node.try_get_context("count") or "1"
-        my_construct.node.apply_aspect(SomeAspect(...))
 
         for i in range(1, int(repo_count)+1):
             code = codecommit.Repository.from_repository_name(
